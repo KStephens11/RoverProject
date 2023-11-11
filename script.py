194f5cb5c80ac8e418b98aa8671da10b83d29e6f
Krakburg <kevinroncolato@outlook.com>
2023-11-11 00:42:31 +0000

    Added openCV for webcam feed, using flask again, html and css changes

--- script.py
+++ script.py
@@ -1,10 +1,41 @@
 # GPIOZERO_PIN_FACTORY=pigpio python3
-# GPIO.cleanup()
+
 from threading import Thread
 from rover import *
-
 import asyncio
 import websockets
+from flask import Flask, render_template, request, Response
+import cv2
+            
+app = Flask(__name__)
+
+def getFrame(cam):
+    ret, frame = cam.read()
+    if ret:
+        ret, jpeg = cv2.imencode('.jpg', frame)
+        return jpeg.tobytes()
+                
+def videoFrames():
+    camera = cv2.VideoCapture(0)
+    while True:
+        frame = getFrame(camera)        
+        if frame == None:
+            camera.release()
+            camera = cv2.VideoCapture(0)
+            frame = getFrame(camera)
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+        else:
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+            
+@app.route('/videoStream')
+def videoStream():
+    return Response(videoFrames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 async def handler(websocket, idk):
     state = True
@@ -39,8 +70,14 @@ async def main():
     async with websockets.serve(handler, "192.168.1.5", 8080):
         await asyncio.Future()
 
+def startSite():
+    app.run(debug=True, threaded=True, use_reloader=False, host='0.0.0.0')
 
 if __name__ == "__main__":
     motorSetup()
+    t = Thread(target=startSite)
+    t.start()
     asyncio.run(main())
-    GPIO.cleanup()
+
+GPIO.cleanup()
+camera.release()
