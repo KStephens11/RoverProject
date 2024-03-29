e474029776977ff9f45fb87d90c39bbb781dffe7
Krakburg <kevinroncolato@outlook.com>
2024-03-29 08:13:08 +0000

    Fixed blue colour tint, Data values set and updates, moved data to bottom, adjusted controls position

--- script.py
+++ script.py
@@ -6,11 +6,36 @@ import cv2
 import time
 from gpiozero import AngularServo, Motor, PWMOutputDevice
 from picamera2 import Picamera2
+import board
+import adafruit_dht
+import adafruit_mpu6050
 
 app = Flask(__name__)
 
 camera = Picamera2()
 camera.start()
+
+dhtDevice = adafruit_dht.DHT11(board.D4)
+mpu = adafruit_mpu6050.MPU6050(board.I2C())
+
+motorBR = Motor(21, 26)
+motorBL = Motor(20, 16)
+motorTR = Motor(22, 25)
+motorTL = Motor(10, 9)
+pwmBR = PWMOutputDevice(13, initial_value=0, frequency=100)
+pwmBL = PWMOutputDevice(12, initial_value=0, frequency=100)
+pwmTL = PWMOutputDevice(11, initial_value=0, frequency=100)
+pwmTR = PWMOutputDevice(27, initial_value=0, frequency=100)
+
+BRservo = AngularServo(19)
+TRservo = AngularServo(6)
+BLservo = AngularServo(5)
+TLservo = AngularServo(7)
+
+pwmTR.value = 0.5
+pwmTL.value = 0.5
+pwmBR.value = 0.5
+pwmBL.value = 0.5
 
 
 # Function to get frame from camera and return as byte
@@ -19,14 +44,17 @@ def getPiFrame():
     frame = camera.capture_array()
     # Flip frame 180 degrees
     rot_frame = cv2.rotate(frame, cv2.ROTATE_180)
+    # Convert frame to from BGR to RGB (Uses XBGR8888)
+    rgb_frame = cv2.cvtColor(rot_frame, cv2.COLOR_BGR2RGB)
     # Encode frame into memory buffer
-    jpeg = cv2.imencode('.jpg', rot_frame)[1]
+    jpeg = cv2.imencode('.jpg', rgb_frame)[1]
     # Convert frame to bytes
     return jpeg.tobytes()
 
 def videoFrames():
     while True:
         frame = getPiFrame()
+        #concats frames one by one and shows result
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
 
@@ -44,32 +72,10 @@ def mapPage():
 
 async def handler(websocket, idk): 
     try:
-        motorBR = Motor(21, 26)
-        motorBL = Motor(20, 16)
-        motorTR = Motor(22, 25)
-        motorTL = Motor(10, 9)
-        pwmBR = PWMOutputDevice(13, initial_value=0, frequency=100)
-        pwmBL = PWMOutputDevice(12, initial_value=0, frequency=100)
-        pwmTL = PWMOutputDevice(11, initial_value=0, frequency=100)
-        pwmTR = PWMOutputDevice(27, initial_value=0, frequency=100)
-
-        BRservo = AngularServo(19)
-        TRservo = AngularServo(6)
-        BLservo = AngularServo(5)
-        TLservo = AngularServo(7)
-
-        pwmTR.value = 0.5
-        pwmTL.value = 0.5
-        pwmBR.value = 0.5
-        pwmBL.value = 0.5
-
         while True:
             message = await websocket.recv()
             if message:
                 code, value = message.split(":")
-                value = float(value)
-                print(code)
-                print(value)
                 match code:
                     case "1":
                         motorTR.stop()
@@ -128,6 +134,9 @@ async def handler(websocket, idk):
                         pwmTL.value = value
                         pwmBR.value = value
                         pwmBL.value = value
+                    case "20":
+                        await websocket.send("X:%.2f, Y:%.2f, Z:%.2f m/s^2" % (mpu.acceleration) + ";" + "\nX:%.2f, Y:%.2f, Z:%.2f rad/s" % (mpu.gyro) + ";" + str(dhtDevice.temperature) + ";" + str(dhtDevice.humidity))
+                        
                 print(message)
     except websockets.ConnectionClosedOK:
             print("CONNECTION CLOSED")
